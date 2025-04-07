3245c2212c9fc83563b2a79823fd2b9b67fcd7ea
himanshu123arya <168516400+himanshu123arya@users.noreply.github.com>
2025-04-07 12:21:45 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import CounterTimer from './CounterTimer';
+function App(){
+    return (
+        <div>
+      <CounterTimer  />
+        </div>
+    )
+}
+export default App;
